f7e9282bffdb257b9d8f12121dedd53ae573433c
Yikuni <2058187089@qq.com>
2023-01-14 08:32:03 +0800

    fix bugs

--- src/main/java/com/yikuni/mc/reflect/common/interceptor/DefaultInterceptorDispatcher.java
+++ src/main/java/com/yikuni/mc/reflect/common/interceptor/DefaultInterceptorDispatcher.java
@@ -30,12 +30,20 @@ public class DefaultInterceptorDispatcher implements InterceptorDispatcher{
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onCommand(PlayerCommandPreprocessEvent event){
-        String command = event.getMessage().substring(1, event.getMessage().indexOf(' '));
+        int end = event.getMessage().indexOf(' ');
+        boolean nullArgs = end == -1;
+        if (end == -1) end = event.getMessage().length();
+        String command = event.getMessage().substring(1, end);
         List<RegisteredInterceptor> interceptorList = interceptorMap.get(command);
         if (interceptorList != null){
-            String[] s = event.getMessage().split(" ");
-            String[] args = new String[s.length - 1];
-            System.arraycopy(s, 1, args, 0, s.length - 1);
+            String[] args;
+            if (nullArgs){
+                args = new String[0];
+            }else {
+                String[] s = event.getMessage().split(" ");
+                args = new String[s.length - 1];
+                System.arraycopy(s, 1, args, 0, s.length - 1);
+            }
             for (RegisteredInterceptor registeredInterceptor : interceptorList) {
                 if (registeredInterceptor.getInterceptor().onCommand(event.getPlayer(), args)){
                     event.getPlayer().sendMessage(ChatColor.RED + "You are not allowed to use this command");
